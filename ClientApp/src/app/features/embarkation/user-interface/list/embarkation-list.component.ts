43d75ab1c5a44bd7415072dc1d269b2f8ec76039
John Sourvinos <johnsourvinos@hotmail.com>
2021-07-21 07:14:24 +0300

    Final changes in embarkation

--- ClientApp/src/app/features/embarkation/user-interface/list/embarkation-list.component.ts
+++ ClientApp/src/app/features/embarkation/user-interface/list/embarkation-list.component.ts
@@ -2,20 +2,18 @@ import { ActivatedRoute, NavigationEnd, Router } from '@angular/router'
 import { Component, ViewChild } from '@angular/core'
 import { DateAdapter } from '@angular/material/core'
 import { Subject } from 'rxjs'
-import { Table } from 'primeng/table'
 import { Title } from '@angular/platform-browser'
 // Custom
 import { ButtonClickService } from 'src/app/shared/services/button-click.service'
 import { EmbarkationCompositeViewModel } from '../../classes/view-models/embarkation-composite-view-model'
 import { EmbarkationService } from '../../classes/services/embarkation.service'
 import { HelperService } from 'src/app/shared/services/helper.service'
-import { InteractionService } from 'src/app/shared/services/interaction.service'
 import { KeyboardShortcuts, Unlisten } from 'src/app/shared/services/keyboard-shortcuts.service'
 import { MessageLabelService } from 'src/app/shared/services/messages-label.service'
 import { MessageSnackbarService } from '../../../../shared/services/messages-snackbar.service'
+import { QrScannerComponent } from 'angular2-qrscanner'
 import { SnackbarService } from 'src/app/shared/services/snackbar.service'
 import { slideFromLeft, slideFromRight } from 'src/app/shared/animations/animations'
-import { QrScannerComponent } from 'angular2-qrscanner'
 
 @Component({
     selector: 'embarkation-list',
@@ -37,22 +35,15 @@ export class EmbarkationListComponent {
     public records: EmbarkationCompositeViewModel
     public searchTerm: string
 
-    private tempArray = []
-
+    private temp = []
     public customers = []
     public drivers = []
-
-    public scannerEnabled: boolean
-    public inputDevices: MediaDeviceInfo[] = []
-    private chosenDevice: any
-    private videoDevices: MediaDeviceInfo[] = []
-    private selectedVideoDevice: MediaDeviceInfo
 
     @ViewChild(QrScannerComponent, { static: false }) qrScannerComponent: QrScannerComponent
 
     //#endregion
 
-    constructor(private activatedRoute: ActivatedRoute, private buttonClickService: ButtonClickService, private dateAdapter: DateAdapter<any>, private helperService: HelperService, private interactionService: InteractionService, private keyboardShortcutsService: KeyboardShortcuts, private messageLabelService: MessageLabelService, private messageSnackbarService: MessageSnackbarService, private router: Router, private snackbarService: SnackbarService, private embarkationervice: EmbarkationService, private titleService: Title) {
+    constructor(private activatedRoute: ActivatedRoute, private buttonClickService: ButtonClickService, private dateAdapter: DateAdapter<any>, private helperService: HelperService, private keyboardShortcutsService: KeyboardShortcuts, private messageLabelService: MessageLabelService, private messageSnackbarService: MessageSnackbarService, private router: Router, private snackbarService: SnackbarService, private embarkationervice: EmbarkationService, private titleService: Title) {
         this.router.events.subscribe((navigation) => {
             if (navigation instanceof NavigationEnd) {
                 this.loadRecords()
@@ -71,10 +62,6 @@ export class EmbarkationListComponent {
         this.getLocale()
     }
 
-    ngAfterViewInit(): void {
-        // this.getVideoDevices()
-    }
-
     ngOnDestroy(): void {
         this.ngUnsubscribe.next()
         this.ngUnsubscribe.unsubscribe()
@@ -84,18 +71,6 @@ export class EmbarkationListComponent {
     //#endregion
 
     //#region public methods
-
-    public onClearAllFilters(table: Table): void {
-        this.searchTerm = ''
-        const input = (<HTMLInputElement>document.querySelectorAll('.p-inputtext')[0])
-        input.value = ''
-        table.reset()
-        table.clear()
-    }
-
-    public onHasRemarks(remarks: string): boolean {
-        return remarks.length > 0 ? true : false
-    }
 
     public onDoEmbarkation(id: number): void {
         this.embarkationervice.boardPassenger(id).subscribe(() => {
@@ -116,15 +91,17 @@ export class EmbarkationListComponent {
         this.router.navigate(['/embarkation'])
     }
 
+    public onHasRemarks(remarks: string): boolean {
+        return remarks.length > 0 ? true : false
+    }
+
     public onHideScanner(): void {
         document.getElementById('video').style.display = 'none'
-        this.scannerEnabled = false
     }
 
     public onShowScanner(): void {
         this.positionVideo()
         this.qrScannerComponent.getMediaDevices().then(devices => {
-            console.log(devices)
             const videoDevices: MediaDeviceInfo[] = []
             for (const device of devices) {
                 if (device.kind.toString() === 'videoinput') {
@@ -147,7 +124,8 @@ export class EmbarkationListComponent {
             }
         })
         this.qrScannerComponent.capturedQr.subscribe((result: any) => {
-            console.log(result)
+            this.onHideScanner()
+            this.filterByTicketNo(result)
         })
     }
 
@@ -181,56 +159,21 @@ export class EmbarkationListComponent {
     }
 
     private getDistinctCustomers(): void {
-        this.tempArray = [... new Set(this.records.embarkation.map(x => x.customer))]
-        this.tempArray.forEach(element => {
+        this.temp = [... new Set(this.records.embarkation.map(x => x.customer))]
+        this.temp.forEach(element => {
             this.customers.push({ label: element, value: element })
         })
     }
 
     private getDistinctDrivers(): void {
-        this.tempArray = [... new Set(this.records.embarkation.map(x => x.driver))]
-        this.tempArray.forEach(element => {
+        this.temp = [... new Set(this.records.embarkation.map(x => x.driver))]
+        this.temp.forEach(element => {
             this.drivers.push({ label: element, value: element })
         })
     }
 
     private getLocale(): void {
         this.dateAdapter.setLocale(this.helperService.readItem("language"))
-    }
-
-    private getVideoDevices(): void {
-        this.qrScannerComponent.getMediaDevices().then(devices => {
-            console.log('Devices', devices)
-            for (const device of devices) {
-                if (device.kind.toString() === 'videoinput') {
-                    this.videoDevices.push(device)
-                }
-            }
-            if (this.videoDevices.length > 0) {
-                // let choosenDev: MediaDeviceInfo
-                for (const dev of this.videoDevices) {
-                    console.log('Video Device', dev)
-                    // choosenDev = dev
-                    this.selectedVideoDevice = dev
-                    console.log('Added video device', this.selectedVideoDevice)
-                }
-                // if (choosenDev) {
-                //     this.qrScannerComponent.chooseCamera.next(choosenDev)
-                // } else {
-                //     console.log('Selected video device', this.videoDevices[0])
-                //     this.qrScannerComponent.chooseCamera.next(this.videoDevices[0])
-                // }
-            }
-        })
-        this.qrScannerComponent.capturedQr.subscribe((result: any) => {
-            console.log(result)
-        })
-        // this.qrScannerComponent.capturedQr.subscribe((result: string) => {
-        //     console.log('Result', result)
-        //     // const input = (<HTMLInputElement>document.querySelectorAll('.p-inputtext')[0])
-        //     // input.value = result
-        //     // this.filterByTicketNo(input.value)
-        // })
     }
 
     private loadRecords(): void {
@@ -274,10 +217,6 @@ export class EmbarkationListComponent {
                 record.isCheckedIn = 'MIX'
             }
         })
-    }
-
-    public getTableHeight(): string {
-        return '200px'
     }
 
     //#endregion
