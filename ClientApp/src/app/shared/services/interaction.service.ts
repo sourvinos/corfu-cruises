5642bacd0e17741d5cd868b28145a62b86e3c854
John Sourvinos <johnsourvinos@hotmail.com>
2021-09-23 11:48:55 +0300

    Interactioin service clean-up

--- ClientApp/src/app/shared/services/interaction.service.ts
+++ ClientApp/src/app/shared/services/interaction.service.ts
@@ -7,49 +7,13 @@ export class InteractionService {
 
     //#region variables
 
-    private _record = new Subject<string[]>()
-    private _reservation = new Subject<string[]>()
-    private _refreshReservationList = new Subject<any>()
-    private _tableRow = new Subject()
     private _refreshMenus = new Subject<any>()
 
-    public record = this._record.asObservable()
-    public reservation = this._reservation.asObservable()
-    public refreshReservationList = this._refreshReservationList.asObservable()
-    public tableRow = this._tableRow.asObservable()
     public refreshMenus = this._refreshMenus.asObservable()
 
     //#endregion
 
     //#region public methods
-
-    /**
-     * Caller(s):
-     *  reservation-form.ts
-     *
-     * Subscribers(s):
-     *  reservation-list.ts
-     *
-     * Description:
-     *  The form tells the list to refresh when a record is saved
-     */
-    public mustRefreshReservationList(): void {
-        this._refreshReservationList.next()
-    }
-
-    /**
-     * Caller(s):
-     *  reservation-form.component.ts
-     * 
-     * Subscriber(s):
-     *  reservation-list.component.ts
-     * 
-     * Description:
-     *  The caller(s) send the id of the deleted record so that the subscriber(s) can find the table row and remove it
-     */
-    public removeTableRow(rowIndex: number): void {
-        this._tableRow.next(rowIndex)
-    }
 
     /**
      * Caller(s):
