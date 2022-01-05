e16c37f8ce692d4ceadefe6a510624cb48601a97
John Sourvinos <johnsourvinos@hotmail.com>
2022-01-05 11:40:00 +0200

    Changes in seeding

--- API/Infrastructure/SeedData/SeedGenders.cs
+++ API/Infrastructure/SeedData/SeedGenders.cs
@@ -11,8 +11,8 @@ namespace API.Infrastructure.SeedData {
             if (!context.Genders.Any()) {
                 List<Gender> genders = new() {
                     new Gender { Id = 1, Description = "MALE", IsActive = true, UserId = "e7e014fd-5608-4936-866e-ec11fc8c16da" },
-                    new Gender { Id = 2, Description = "FEMALE", IsActive = false, UserId = "544c9930-ad76-4aa9-bb1c-8dd193508e05" },
-                    new Gender { Id = 3, Description = "OTHER", IsActive = true, UserId = "e7e014fd-5608-4936-866e-ec11fc8c16da" }
+                    new Gender { Id = 2, Description = "FEMALE", IsActive = true, UserId = "544c9930-ad76-4aa9-bb1c-8dd193508e05" },
+                    new Gender { Id = 3, Description = "OTHER", IsActive = false, UserId = "e7e014fd-5608-4936-866e-ec11fc8c16da" }
                 };
                 context.AddRange(genders);
                 context.SaveChanges();
--- API/Infrastructure/SeedData/SeedOccupants.cs
+++ API/Infrastructure/SeedData/SeedOccupants.cs
@@ -11,7 +11,8 @@ namespace API.Infrastructure.SeedData {
             if (!context.Occupants.Any()) {
                 List<Occupant> occupants = new() {
                     new Occupant { Id = 1, Description = "CREW", IsActive = true, UserId = "e7e014fd-5608-4936-866e-ec11fc8c16da" },
-                    new Occupant { Id = 2, Description = "PASSENGER", IsActive = false, UserId = "e7e014fd-5608-4936-866e-ec11fc8c16da" }
+                    new Occupant { Id = 2, Description = "PASSENGER", IsActive = true, UserId = "e7e014fd-5608-4936-866e-ec11fc8c16da" },
+                    new Occupant { Id = 3, Description = "OTHER", IsActive = false, UserId = "e7e014fd-5608-4936-866e-ec11fc8c16da" }
                 };
                 context.AddRange(occupants);
                 context.SaveChanges();
