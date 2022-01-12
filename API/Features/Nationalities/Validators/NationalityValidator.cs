a35180b66ed965464c010185b4f8106b5b100df9
John Sourvinos <johnsourvinos@hotmail.com>
2022-01-12 16:47:26 +0200

    Adds unit tests for nationalities

--- API/Features/Nationalities/Validators/NationalityValidator.cs
+++ API/Features/Nationalities/Validators/NationalityValidator.cs
@@ -5,8 +5,8 @@ namespace API.Features.Nationalities {
     public class NationalityValidator : AbstractValidator<NationalityWriteResource> {
 
         public NationalityValidator() {
-            RuleFor(x => x.Code).NotEmpty().MaximumLength(10);
             RuleFor(x => x.Description).NotEmpty().MaximumLength(128);
+            RuleFor(x => x.Code).NotEmpty().MaximumLength(10);
         }
 
     }
