b8d82054d328242f05db33963797613da8217f43
John Sourvinos <johnsourvinos@hotmail.com>
2022-01-23 10:46:11 +0200

    Changes in ship ships integration tests

--- API.Integration.Tests/Features/Ships/Controller/Ships06Delete.cs
+++ API.Integration.Tests/Features/Ships/Controller/Ships06Delete.cs
@@ -60,17 +60,16 @@ namespace API.IntegrationTests.Ships {
             Assert.Equal(HttpStatusCode.Unauthorized, actionResponse.StatusCode);
         }
 
-        [Theory]
-        [ClassData(typeof(ActiveUsersCanLogin))]
-        public async Task Not_Found_When_Not_Exists(Login login) {
+        [Fact]
+        public async Task Active_Admins_Not_Found_When_Not_Exists() {
             // arrange
-            var loginResponse = await Helpers.Login(_httpClient, Helpers.CreateLoginCredentials(login.Username, login.Password));
+            var loginResponse = await Helpers.Login(_httpClient, Helpers.CreateLoginCredentials("john", "ec11fc8c16da"));
             _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue(JwtBearerDefaults.AuthenticationScheme, loginResponse.Token);
             var request = Helpers.CreateRequest(_baseUrl, _notFoundUrl);
             // act
             var actionResponse = await _httpClient.SendAsync(request);
             // assert
-            Assert.Equal((HttpStatusCode)login.StatusCode, actionResponse.StatusCode);
+            Assert.Equal(HttpStatusCode.NotFound, actionResponse.StatusCode);
             // cleanup
             await Helpers.Logout(_httpClient, loginResponse.UserId);
         }
