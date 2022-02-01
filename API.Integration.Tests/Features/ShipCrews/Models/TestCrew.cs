98bc7ad5cd1f3532839baf9bfdd64d226ef9f86f
John Sourvinos <johnsourvinos@hotmail.com>
2022-02-01 09:31:06 +0200

    Rewrite of integration tests, crews

---
+++ API.Integration.Tests/Features/ShipCrews/Models/TestCrew.cs
@@ -0,0 +1,19 @@
+using API.Integration.Tests.Infrastructure;
+
+namespace API.IntegrationTests.ShipCrews {
+
+    public class TestCrew : ITestEntity {
+
+        public int StatusCode { get; set; }
+
+        public int Id { get; set; }
+        public int GenderId { get; set; }
+        public int NationalityId { get; set; }
+        public int ShipId { get; set; }
+        public string Lastname { get; set; }
+        public string Firstname { get; set; }
+        public string Birthdate { get; set; }
+
+    }
+
+}
