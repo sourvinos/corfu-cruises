00a00d53e850c38d7f5134743d0d8b057f22dd17
John Sourvinos <johnsourvinos@hotmail.com>
2022-03-05 08:56:04 +0200

    Changes in side menu

--- FrontEnd/src/app/shared/components/side-bar-wrapper/side-menu/side-menu.component.ts
+++ FrontEnd/src/app/shared/components/side-bar-wrapper/side-menu/side-menu.component.ts
@@ -5,7 +5,6 @@ import { Observable, Subject } from 'rxjs'
 import { takeUntil } from 'rxjs/operators'
 // Custom
 import { AccountService } from '../../../services/account.service'
-import { HelperService } from 'src/app/shared/services/helper.service'
 import { InteractionService } from 'src/app/shared/services/interaction.service'
 import { MessageMenuService } from '../../../services/messages-menu.service'
 import { Router } from '@angular/router'
@@ -27,7 +26,7 @@ export class SideMenuComponent {
 
     //#endregion
 
-    constructor(private accountService: AccountService, private helperService: HelperService, private interactionService: InteractionService, private messageMenuService: MessageMenuService, private router: Router) { }
+    constructor(private accountService: AccountService, private interactionService: InteractionService, private messageMenuService: MessageMenuService, private router: Router) { }
 
     //#region lifecycle hooks
 
@@ -54,39 +53,39 @@ export class SideMenuComponent {
                 routerLink: ['/']
             },
             {
-                label: this.getLabel(menuItems, 'passengers'),
-                icon: 'fas fa-users',
-                items: [
-                    { label: this.getLabel(menuItems, 'embarkation'), command: (): void => { this.router.navigate(['/']) } },
-                    { label: this.getLabel(menuItems, 'manifest'), command: (): void => { this.router.navigate(['/']) } },
-                ]
+                label: this.getLabel(menuItems, 'embarkation'), command: (): void => { this.router.navigate(['/embarkation']) },
+                icon: 'fas fa-ship'
             },
             {
-                label: this.getLabel(menuItems, 'reservations'),
-                icon: 'fab fa-buffer',
+                label: this.getLabel(menuItems, 'reservations'), command: (): void => { this.router.navigate(['/reservations']) },
+                icon: 'fas fa-ticket-alt'
+            },
+            {
+                label: this.getLabel(menuItems, 'tasks'),
+                icon: 'fas fa-microchip',
                 items: [
-                    { label: this.getLabel(menuItems, 'dashboard'), command: (): void => { this.router.navigate(['/reservations']) } },
-                    { label: this.getLabel(menuItems, 'invoicing'), command: (): void => { this.router.navigate(['/']) } },
+                    { label: this.getLabel(menuItems, 'invoicing', true), command: (): void => { this.router.navigate(['/']) } },
+                    { label: this.getLabel(menuItems, 'manifest', true), command: (): void => { this.router.navigate(['/']) } },
                 ]
             },
             {
                 label: this.getLabel(menuItems, 'tables'),
-                icon: 'fas fa-table',
+                icon: 'fas fa-list-alt',
                 items: [
-                    { label: this.getLabel(menuItems, 'customers'), command: (): void => { this.router.navigate(['/customers']) } },
-                    { label: this.getLabel(menuItems, 'destinations'), command: (): void => { this.router.navigate(['/destinations']) } },
-                    { label: this.getLabel(menuItems, 'drivers'), command: (): void => { this.router.navigate(['/drivers']) } },
-                    { label: this.getLabel(menuItems, 'pickupPoints'), command: (): void => { this.router.navigate(['/pickupPoints']) } },
-                    { label: this.getLabel(menuItems, 'coachRoutes'), command: (): void => { this.router.navigate(['/routes']) } },
-                    { label: this.getLabel(menuItems, 'ports'), command: (): void => { this.router.navigate(['/ports']) } },
-                    { label: this.getLabel(menuItems, 'schedules'), command: (): void => { this.router.navigate(['/schedules']) } },
-                    { label: this.getLabel(menuItems, 'ships'), command: (): void => { this.router.navigate(['/ships']) } },
-                    { label: this.getLabel(menuItems, 'crews'), command: (): void => { this.router.navigate(['/shipCrews']) } },
-                    { label: this.getLabel(menuItems, 'genders'), command: (): void => { this.router.navigate(['/genders']) } },
-                    { label: this.getLabel(menuItems, 'registrars'), command: (): void => { this.router.navigate(['/shipRegistrars']) } },
-                    { label: this.getLabel(menuItems, 'shipOwners'), command: (): void => { this.router.navigate(['/shipOwners']) } },
-                    { label: this.getLabel(menuItems, 'shipRoutes'), command: (): void => { this.router.navigate(['/shipRoutes']) } },
-                    { label: this.getLabel(menuItems, 'users'), command: (): void => { this.router.navigate(['/users']) } }
+                    { label: this.getLabel(menuItems, 'customers', true), command: (): void => { this.router.navigate(['/customers']) } },
+                    { label: this.getLabel(menuItems, 'destinations', true), command: (): void => { this.router.navigate(['/destinations']) } },
+                    { label: this.getLabel(menuItems, 'drivers', true), command: (): void => { this.router.navigate(['/drivers']) } },
+                    { label: this.getLabel(menuItems, 'pickupPoints', true), command: (): void => { this.router.navigate(['/pickupPoints']) } },
+                    { label: this.getLabel(menuItems, 'coachRoutes', true), command: (): void => { this.router.navigate(['/routes']) } },
+                    { label: this.getLabel(menuItems, 'ports', true), command: (): void => { this.router.navigate(['/ports']) } },
+                    { label: this.getLabel(menuItems, 'schedules', true), command: (): void => { this.router.navigate(['/schedules']) } },
+                    { label: this.getLabel(menuItems, 'ships', true), command: (): void => { this.router.navigate(['/ships']) } },
+                    { label: this.getLabel(menuItems, 'crews', true), command: (): void => { this.router.navigate(['/shipCrews']) } },
+                    { label: this.getLabel(menuItems, 'genders', true), command: (): void => { this.router.navigate(['/genders']) } },
+                    { label: this.getLabel(menuItems, 'registrars', true), command: (): void => { this.router.navigate(['/shipRegistrars']) } },
+                    { label: this.getLabel(menuItems, 'shipOwners', true), command: (): void => { this.router.navigate(['/shipOwners']) } },
+                    { label: this.getLabel(menuItems, 'shipRoutes', true), command: (): void => { this.router.navigate(['/shipRoutes']) } },
+                    { label: this.getLabel(menuItems, 'users', true), command: (): void => { this.router.navigate(['/users']) } }
                 ]
             }
         ]
@@ -103,8 +102,8 @@ export class SideMenuComponent {
         ]
     }
 
-    private getLabel(response: any[], label: string): string {
-        return this.messageMenuService.getDescription(response, 'menus', label)
+    private getLabel(response: any[], label: string, showIcon = false): string {
+        return showIcon ? this.getIcon() + this.messageMenuService.getDescription(response, 'menus', label) : this.messageMenuService.getDescription(response, 'menus', label)
     }
 
     private subscribeToInteractionService(): void {
@@ -117,6 +116,10 @@ export class SideMenuComponent {
 
     private updateVariables(): void {
         this.loginStatus = this.accountService.isLoggedIn
+    }
+
+    private getIcon(): string {
+        return '◻️ '
     }
 
     //#endregion
