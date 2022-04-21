8af914cdfbf69f06b4c1b9d0defca74064b40f76
John Sourvinos <johnsourvinos@hotmail.com>
2022-04-21 08:09:57 +0300

    Disables theme selection

--- FrontEnd/src/app/shared/components/top-bar-wrapper/theme-menu/theme-menu.component.ts
+++ FrontEnd/src/app/shared/components/top-bar-wrapper/theme-menu/theme-menu.component.ts
@@ -1,7 +1,6 @@
 import { Component, HostListener, Inject } from '@angular/core'
 import { DOCUMENT } from '@angular/common'
 // Custom
-import { LocalStorageService } from './../../../services/local-storage.service'
 import { MessageLabelService } from 'src/app/shared/services/messages-label.service'
 
 @Component({
@@ -19,7 +18,7 @@ export class ThemeMenuComponent {
 
     //#endregion
 
-    constructor(@Inject(DOCUMENT) private document: Document, private localStorageService: LocalStorageService, private messageLabelService: MessageLabelService) { }
+    constructor(@Inject(DOCUMENT) private document: Document, private messageLabelService: MessageLabelService) { }
 
     @HostListener('mouseenter') onMouseEnter(): void {
         document.querySelectorAll('.sub-menu').forEach((item) => {
@@ -37,18 +36,12 @@ export class ThemeMenuComponent {
 
     //#region public methods
 
-    public changeTheme(theme: string): void {
-        this.setDefaultTheme(theme)
-        this.attachStylesheetToHead()
-        this.saveSelectedTheme()
-    }
-
     public getLabel(id: string): string {
         return this.messageLabelService.getDescription(this.feature, id)
     }
 
-    public getTheme(): string {
-        return this.localStorageService.getItem('theme') == '' ? this.onSaveTheme(this.defaultTheme) : this.localStorageService.getItem('theme')
+    public getSelectedThemeIcon(): string {
+        return this.defaultTheme
     }
 
     public hideMenu(): void {
@@ -64,7 +57,6 @@ export class ThemeMenuComponent {
     private applyTheme(): void {
         this.setSelectedTheme()
         this.attachStylesheetToHead()
-        this.saveSelectedTheme()
     }
 
     private attachStylesheetToHead(): void {
@@ -75,21 +67,8 @@ export class ThemeMenuComponent {
         headElement.appendChild(newLinkElement)
     }
 
-    private setDefaultTheme(theme: string): void {
-        this.defaultTheme = theme
-    }
-
-    private saveSelectedTheme(): void {
-        this.localStorageService.saveItem('theme', this.defaultTheme)
-    }
-
     private setSelectedTheme(): void {
-        this.defaultTheme = this.localStorageService.getItem('theme') || this.defaultTheme
-    }
-
-    public onSaveTheme(theme: string): string {
-        this.localStorageService.saveItem('theme', theme)
-        return theme
+        this.defaultTheme = 'blue'
     }
 
     //#endregion
