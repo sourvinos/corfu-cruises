1aba45d2e60602ad8079476b137bbe7f9a0832ac
John Sourvinos <johnsourvinos@hotmail.com>
2022-06-26 20:04:39 +0300

    Removes NLog

--- API/Program.cs
+++ API/Program.cs
@@ -1,37 +1,19 @@
-using System;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using NLog;
-using NLog.Web;
 
 namespace API {
 
     public class Program {
 
         public static void Main(string[] args) {
-            var logger = LogManager.Setup().LoadConfigurationFromAppSettings().GetCurrentClassLogger();
-            try {
-                logger.Debug("Init...");
-                CreateHostBuilder(args).Build().Run();
-            } catch (Exception exception) {
-                logger.Error(exception, "An exception has stopped the program from starting...");
-                throw;
-            } finally {
-                LogManager.Shutdown();
-            }
+            CreateHostBuilder(args).Build().Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) {
             return Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder => {
                     webBuilder.UseStartup<Startup>();
-                })
-                .ConfigureLogging(logging => {
-                    logging.ClearProviders();
-                    logging.SetMinimumLevel(Microsoft.Extensions.Logging.LogLevel.Trace);
-                })
-                .UseNLog();
+                });
         }
 
     }
