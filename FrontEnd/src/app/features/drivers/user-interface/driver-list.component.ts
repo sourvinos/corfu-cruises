ef90954ff018bd9d940f2ae048c2ea0c2615d932
John Sourvinos <johnsourvinos@hotmail.com>
2022-11-02 18:58:38 +0200

    Driver list

--- FrontEnd/src/app/features/drivers/user-interface/driver-list.component.ts
+++ FrontEnd/src/app/features/drivers/user-interface/driver-list.component.ts
@@ -1,13 +1,14 @@
 import { ActivatedRoute, Router } from '@angular/router'
 import { Component } from '@angular/core'
 // Custom
-import { ButtonClickService } from 'src/app/shared/services/button-click.service'
 import { HelperService } from 'src/app/shared/services/helper.service'
-import { KeyboardShortcuts, Unlisten } from 'src/app/shared/services/keyboard-shortcuts.service'
 import { ListResolved } from 'src/app/shared/classes/list-resolved'
 import { MessageLabelService } from 'src/app/shared/services/messages-label.service'
 import { MessageSnackbarService } from 'src/app/shared/services/messages-snackbar.service'
 import { ModalActionResultService } from 'src/app/shared/services/modal-action-result.service'
+import { Subject } from 'rxjs'
+import { DriverListVM } from '../classes/view-models/driver-list-vm'
+import { environment } from 'src/environments/environment'
 
 @Component({
     selector: 'driver-list',
@@ -19,30 +20,27 @@ export class DriverListComponent {
 
     //#region variables
 
-    private unlisten: Unlisten
+    private unsubscribe = new Subject<void>()
     private url = 'drivers'
-    public drivers = []
     public feature = 'driverList'
     public icon = 'home'
     public parentUrl = '/'
+    public records: DriverListVM[] = []
+    public recordsFiltered: DriverListVM[] = []
 
     //#endregion
 
-    constructor(private activatedRoute: ActivatedRoute, private buttonClickService: ButtonClickService, private helperService: HelperService, private keyboardShortcutsService: KeyboardShortcuts, private messageLabelService: MessageLabelService, private messageSnackbarService: MessageSnackbarService, private modalActionResultService: ModalActionResultService, private router: Router) { }
+    constructor(private activatedRoute: ActivatedRoute, private helperService: HelperService, private messageLabelService: MessageLabelService, private messageSnackbarService: MessageSnackbarService, private modalActionResultService: ModalActionResultService, private router: Router) { }
 
     //#region lifecycle hooks
 
     ngOnInit(): void {
-        this.addShortcuts()
         this.loadRecords()
-    }
-
-    ngAfterViewInit(): void {
-        this.changeScrollWheelSpeed()
+        this.calculateTableHeight()
     }
 
     ngOnDestroy(): void {
-        this.unlisten()
+        this.cleanup()
     }
 
     //#endregion
@@ -51,6 +49,14 @@ export class DriverListComponent {
 
     public editRecord(id: number): void {
         this.router.navigate([this.url, id])
+    }
+
+    public filterRecords(event: { filteredValue: any[] }) {
+        this.recordsFiltered = event.filteredValue
+    }
+
+    public getIcon(filename: string): string {
+        return environment.criteriaIconDirectory + filename + '.svg'
     }
 
     public getLabel(id: string): string {
@@ -65,22 +71,15 @@ export class DriverListComponent {
 
     //#region private methods
 
-    private addShortcuts(): void {
-        this.unlisten = this.keyboardShortcutsService.listen({
-            'Escape': () => {
-                this.goBack()
-            },
-            'Alt.N': (event: KeyboardEvent) => {
-                this.buttonClickService.clickOnButton(event, 'new')
-            }
-        }, {
-            priority: 0,
-            inputs: true
-        })
+    private calculateTableHeight(): void {
+        setTimeout(() => {
+            document.getElementById('table-wrapper').style.height = this.helperService.calculateTableWrapperHeight('top-bar', 'header', 'footer')
+        }, 500)
     }
 
-    private changeScrollWheelSpeed(): void {
-        this.helperService.changeScrollWheelSpeed(document.querySelector<HTMLElement>('.cdk-virtual-scroll-viewport'))
+    private cleanup(): void {
+        this.unsubscribe.next()
+        this.unsubscribe.unsubscribe()
     }
 
     private goBack(): void {
@@ -91,8 +90,9 @@ export class DriverListComponent {
         const promise = new Promise((resolve) => {
             const listResolved: ListResolved = this.activatedRoute.snapshot.data[this.feature]
             if (listResolved.error === null) {
-                this.drivers = listResolved.list
-                resolve(this.drivers)
+                this.records = listResolved.list
+                this.recordsFiltered = listResolved.list
+                resolve(this.records)
             } else {
                 this.goBack()
                 this.modalActionResultService.open(this.messageSnackbarService.filterResponse(new Error('500')), 'error', ['ok'])
