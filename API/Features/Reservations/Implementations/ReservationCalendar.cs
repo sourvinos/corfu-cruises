e1ac587d1773f1c59034f54459f37f3cd480902d
John Sourvinos <johnsourvinos@hotmail.com>
2022-11-09 10:31:48 +0200

    Fixed calendar for the reservations API

--- API/Features/Reservations/Implementations/ReservationCalendar.cs
+++ API/Features/Reservations/Implementations/ReservationCalendar.cs
@@ -15,22 +15,55 @@ namespace API.Features.Reservations {
 
         public ReservationCalendar(AppDbContext context, IHttpContextAccessor httpContext, IOptions<TestingEnvironment> testingEnvironment) : base(context, httpContext, testingEnvironment) { }
 
+        /// <summary>
+        ///     Creates an array of ReservationCalendarGroupVM objects. It iterates the period array.
+        ///     Each object contains:
+        ///         Date: The date taken from the schedules table.
+        ///         Destinations: An array of DestinationCalendarVM objects taken from the schedules table.
+        ///         Pax: The sum of persons for the date and destination taken from the reservatoins table.
+        /// </summary>
+        /// <param name="fromDate"></param>
+        /// <param name="toDate"></param>
+        /// <returns>
+        ///     An array of ReservationCalendarGroupVM objects, one object for each day.
+        /// </returns>
         public async Task<IEnumerable<ReservationCalendarGroupVM>> GetForCalendarAsync(string fromDate, string toDate) {
-            return await context.Schedules
-                .AsNoTracking()
-                .Include(x => x.Reservations)
-                .Where(x => x.Date >= Convert.ToDateTime(fromDate) && x.Date <= Convert.ToDateTime(toDate))
-                .GroupBy(z => new { z.Date })
-                .Select(x => new ReservationCalendarGroupVM {
-                    Date = DateHelpers.DateToISOString(x.Key.Date),
-                    Destinations = x.GroupBy(v => new { v.Date, v.Destination.Id, v.Destination.Abbreviation, v.Destination.Description }).Select(x => new DestinationCalendarVM {
-                        Id = x.Key.Id,
-                        Abbreviation = x.Key.Abbreviation,
-                        Description = x.Key.Description,
-                        Pax = 0
-                    }),
-                    Pax = 0
-                }).ToListAsync();
+            var schedules = new List<ReservationCalendarGroupVM>();
+            foreach (var date in BuildPeriod(fromDate, toDate)) {
+                var x = await context.Schedules
+                    .AsNoTracking()
+                    .Where(x => x.Date == Convert.ToDateTime(date))
+                    .GroupBy(z => new { z.Date })
+                    .Select(x => new ReservationCalendarGroupVM {
+                        Date = date,
+                        Destinations = x.GroupBy(v => new { v.Destination.Id, v.Destination.Abbreviation, v.Destination.Description }).Select(x => new DestinationCalendarVM {
+                            Id = x.Key.Id,
+                            Abbreviation = x.Key.Abbreviation,
+                            Description = x.Key.Description,
+                            Pax = context.Reservations.Where(z => z.Date == Convert.ToDateTime(date) && z.DestinationId == x.Key.Id).Sum(x => x.TotalPersons)
+                        }),
+                        Pax = context.Reservations.Where(z => z.Date == x.Key.Date).Sum(x => x.TotalPersons)
+                    }).FirstOrDefaultAsync();
+                schedules.Add(x);
+            }
+            return schedules;
+        }
+
+        /// <summary>
+        ///     Builds an array of strings containing the dates between the beginning and the end of the period
+        /// </summary>
+        /// <param name="fromDate"></param>
+        /// <param name="toDate"></param>
+        /// <returns>
+        ///     An array of strings
+        /// </returns>
+        private static string[] BuildPeriod(string fromDate, string toDate) {
+            var days = (DateTime.Parse(toDate) - DateTime.Parse(fromDate)).TotalDays + 1;
+            var period = new string[(int)days];
+            for (int i = 0; i < days; i++) {
+                period[i] = DateHelpers.DateToISOString(DateTime.Parse(fromDate).AddDays(i));
+            }
+            return period;
         }
 
     }
