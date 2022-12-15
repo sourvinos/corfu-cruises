f1a538d90fdcfd61f4282ee093d8ae6317ceb3f3
John Sourvinos <johnsourvinos@hotmail.com>
2022-12-15 06:52:45 +0200

    Ships store filters with virtual scrolling

--- API/Features/Ships/ViewModels/ShipListVM.cs
+++ API/Features/Ships/ViewModels/ShipListVM.cs
@@ -4,6 +4,7 @@ namespace API.Features.Ships {
 
         public int Id { get; set; }
         public string Description { get; set; }
+        public string IMO { get; set; }
         public bool IsActive { get; set; }
 
     }
