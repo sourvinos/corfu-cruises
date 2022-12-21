83b193f183ba5b40f2d10b712528dba38b434345
John Sourvinos <johnsourvinos@hotmail.com>
2022-12-21 16:52:02 +0200

    New reservations year selector

--- FrontEnd/src/app/shared/components/year-selector/year-selector.component.ts
+++ FrontEnd/src/app/shared/components/year-selector/year-selector.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, EventEmitter, Input, Output, ViewChild } from '@angular/core'
+import { Component, EventEmitter, HostListener, Input, Output } from '@angular/core'
 import { environment } from 'src/environments/environment'
 
 @Component({
@@ -9,52 +9,59 @@ import { environment } from 'src/environments/environment'
 
 export class YearSelectorComponent {
 
-    @Input() public activeYear: number
+    //#region variables
 
+    @Input() public activeYear: number
     @Output() public yearEmitter = new EventEmitter()
 
-    @ViewChild('yearOpener', { static: false }) yearOpener: ElementRef<HTMLInputElement>
+    public years: string[] = []
 
-    public years: string[]
-    public imgIsLoaded = false
-    // public isOpen = false
+    //#endregion
 
-    ngOnInit(): void {
-        this.populateMonths()
+    //#region listeners
+
+    @HostListener('mouseenter') onMouseEnter(): void {
+        document.querySelectorAll('.sub-menu').forEach((item) => {
+            item.classList.remove('hidden')
+        })
     }
 
+    //#endregion
+
+    //#region lifecycle hooks
+
+    ngOnInit(): void {
+        this.populateYears()
+    }
+
+    //#endregion
+
+    //#region public methods
 
     public getIcon(filename: string): string {
         return environment.calendarIconDirectory + filename + '.svg'
     }
 
-    public imageIsLoading(): any {
-        return this.imgIsLoaded ? '' : 'skeleton'
-    }
-
-    public loadImage(): void {
-        this.imgIsLoaded = true
+    public hideMenu(): void {
+        document.querySelectorAll('.sub-menu').forEach((item) => {
+            item.classList.add('hidden')
+        })
     }
 
     public selectYear(year: string): any {
-        this.yearOpener.nativeElement.checked = false
-        // this.isOpen = false
         this.yearEmitter.emit(year)
     }
 
-    public setOpenerVisibility(visible: boolean): void {
-        this.yearOpener.nativeElement.checked = visible
-        // this.isOpen = !this.isOpen
+    //#endregion
+
+    //#region private methods
+
+    private populateYears(): void {
+        for (let year = 2022; year < 2028; year++) {
+            this.years.push(year.toString())
+        }
     }
 
-    public getOpenerVisibility(): boolean {
-        return this.yearOpener != undefined ? this.yearOpener.nativeElement.checked : false
-    }
-
-    private populateMonths(): void {
-        this.years = [
-            '2021', '2022', '2023', '2024'
-        ]
-    }
+    //#endregion
 
 }
