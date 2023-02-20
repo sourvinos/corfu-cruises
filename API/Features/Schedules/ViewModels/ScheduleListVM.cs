bcc1f60c0fad5480ee05482b5dd051e18ac22294
John Sourvinos <johnsourvinos@hotmail.com>
2023-02-20 11:58:24 +0200

    Schedules list revisited with possible virtual fix!

--- API/Features/Schedules/ViewModels/ScheduleListVM.cs
+++ API/Features/Schedules/ViewModels/ScheduleListVM.cs
@@ -1,11 +1,13 @@
+using API.Infrastructure.Classes;
+
 namespace API.Features.Schedules {
 
     public class ScheduleListVM {
 
         public int Id { get; set; }
         public string Date { get; set; }
-        public string DestinationDescription { get; set; }
-        public string PortDescription { get; set; }
+        public SimpleEntity Destination { get; set; }
+        public SimpleEntity Port { get; set; }
         public int MaxPax { get; set; }
         public bool IsActive { get; set; }
 
--- FrontEnd/src/app/features/schedules/user-interface/list/schedule-list.component.ts
+++ FrontEnd/src/app/features/schedules/user-interface/list/schedule-list.component.ts
@@ -6,6 +6,7 @@ import { Subject, takeUntil } from 'rxjs'
 import { Table } from 'primeng/table'
 // Custom
 import { DateHelperService } from 'src/app/shared/services/date-helper.service'
+import { EmojiService } from 'src/app/shared/services/emoji.service'
 import { HelperService } from 'src/app/shared/services/helper.service'
 import { InteractionService } from 'src/app/shared/services/interaction.service'
 import { ListResolved } from 'src/app/shared/classes/list-resolved'
@@ -34,28 +35,35 @@ export class ScheduleListComponent {
     public icon = 'home'
     public parentUrl = '/'
     public records: ScheduleListVM[] = []
-    public recordsFiltered: ScheduleListVM[] = []
+    public recordsFilteredCount: number
+    public isVirtual = true
+    private scrollableElement: any
 
-    public dropdownDate = ''
-    public dropdownDestinations = []
-    public dropdownPorts = []
+    public filterDate = ''
+    public distinctDestinations = []
+    public distinctPorts = []
 
     //#endregion
 
-    constructor(private activatedRoute: ActivatedRoute, private dateAdapter: DateAdapter<any>, private dateHelperService: DateHelperService, private helperService: HelperService, private interactionService: InteractionService, private localStorageService: LocalStorageService, private messageLabelService: MessageLabelService, private messageSnackbarService: MessageSnackbarService, private modalActionResultService: ModalActionResultService, private router: Router) { }
+    constructor(private activatedRoute: ActivatedRoute, private dateAdapter: DateAdapter<any>, private dateHelperService: DateHelperService, private emojiService: EmojiService, private helperService: HelperService, private interactionService: InteractionService, private localStorageService: LocalStorageService, private messageLabelService: MessageLabelService, private messageSnackbarService: MessageSnackbarService, private modalActionResultService: ModalActionResultService, private router: Router) {
+        this.loadRecords().then(() => {
+            this.populateDropdownFilters()
+            this.filterTableFromStoredFilters()
+            this.formatDatesToLocale()
+            this.enableDisableFilters()
+            this.subscribeToInteractionService()
+            this.setLocale()
+        })
+    }
 
     //#region lifecycle hooks
 
-    ngOnInit(): void {
-        this.loadRecords()
-        this.formatDatesToLocale()
-        this.populateDropdownFilters()
-        this.subscribeToInteractionService()
-        this.setLocale()
-    }
-
     ngAfterViewInit(): void {
-        this.filterTableFromStoredFilters()
+        setTimeout(() => {
+            this.getScrollableElement()
+            this.toggleVirtual()
+            this.hightlightSavedRow()
+        }, 500)
     }
 
     ngOnDestroy(): void {
@@ -68,24 +76,32 @@ export class ScheduleListComponent {
 
     public clearDateFilter(): void {
         this.table.filter('', 'date', 'equals')
-        this.dropdownDate = ''
+        this.filterDate = ''
         this.localStorageService.saveItem(this.feature, JSON.stringify(this.table.filters))
     }
 
     public editRecord(id: number): void {
+        this.storeScrollTop()
+        this.storeSelectedId(id)
         this.router.navigate([this.url, id])
     }
 
     public filterByDate(event: MatDatepickerInputEvent<Date>): void {
-        const date = this.dateHelperService.formatDateToIso(event.value, false)
+        const date = this.dateHelperService.formatDateToIso(new Date(event.value), false)
         this.table.filter(date, 'date', 'equals')
-        this.dropdownDate = date
+        this.filterDate = date
         this.localStorageService.saveItem(this.feature, JSON.stringify(this.table.filters))
     }
 
     public filterRecords(event: { filteredValue: any[] }): void {
-        this.recordsFiltered = event.filteredValue
         this.localStorageService.saveItem(this.feature, JSON.stringify(this.table.filters))
+        this.recordsFilteredCount = event.filteredValue.length
+        const x = document.getElementsByClassName('p-scroller-content') as HTMLCollectionOf<HTMLInputElement>
+        x[0].style.transform = null
+    }
+
+    public getEmoji(emoji: string): string {
+        return this.emojiService.getEmoji(emoji)
     }
 
     public getLabel(id: string): string {
@@ -93,7 +109,7 @@ export class ScheduleListComponent {
     }
 
     public hasDateFilter(): string {
-        return this.dropdownDate == '' ? 'hidden' : ''
+        return this.filterDate == '' ? 'hidden' : ''
     }
 
     public newRecord(): void {
@@ -101,8 +117,12 @@ export class ScheduleListComponent {
     }
 
     public resetTableFilters(): void {
-        this.dropdownDate = ''
+        this.filterDate = ''
         this.helperService.clearTableTextFilters(this.table, ['date', 'maxPax'])
+    }
+
+    public unHighlightAllRows(): void {
+        this.helperService.unHighlightAllRows()
     }
 
     //#endregion
@@ -112,6 +132,13 @@ export class ScheduleListComponent {
     private cleanup(): void {
         this.unsubscribe.next()
         this.unsubscribe.unsubscribe()
+    }
+
+    private enableDisableFilters(): void {
+        if (this.records.length == 0) {
+            this.helperService.disableTableDropdownFilters()
+            this.helperService.disableTableTextFilters()
+        }
     }
 
     private filterColumn(element: { value: any }, field: string, matchMode: string): void {
@@ -126,12 +153,12 @@ export class ScheduleListComponent {
             setTimeout(() => {
                 this.filterColumn(filters.isActive, 'isActive', 'contains')
                 this.filterColumn(filters.date, 'date', 'equals')
-                this.filterColumn(filters.destinationDescription, 'destinationDescription', 'equals')
-                this.filterColumn(filters.portDescription, 'portDescription', 'equals')
+                this.filterColumn(filters.destination, 'destination', 'in')
+                this.filterColumn(filters.port, 'port', 'in')
                 this.filterColumn(filters.maxPax, 'maxPax', 'contains')
                 if (filters.date != undefined) {
                     const date = new Date(Date.parse(filters.date.value))
-                    this.dropdownDate = this.dateAdapter.createDate(date.getFullYear(), date.getMonth(), parseInt(date.getDate().toLocaleString()))
+                    this.filterDate = this.dateAdapter.createDate(date.getFullYear(), date.getMonth(), parseInt(date.getDate().toLocaleString()))
                 }
             }, 500)
         }
@@ -143,8 +170,33 @@ export class ScheduleListComponent {
         })
     }
 
+    private getScrollableElement(): void {
+        // this.scrollableElement = document.getElementsByClassName('p-datatable-wrapper')[0]
+        this.scrollableElement = document.getElementsByClassName('p-scroller-inline')[0]
+    }
+
     private goBack(): void {
         this.router.navigate([this.parentUrl])
+    }
+
+    private hightlightSavedRow(): void {
+        setTimeout(() => {
+            document.getElementsByClassName('p-scroller-inline')[0]?.scrollTo({
+                top: parseInt(this.localStorageService.getItem('scrollTop')) || 0,
+                left: 0,
+                behavior: 'auto'
+            })
+        }, 1000)
+        setTimeout(() => {
+            const x = document.getElementById(this.localStorageService.getItem('id'))
+            x.classList.add('p-highlight')
+        }, 1000)
+
+
+        // setTimeout(() => {
+        //     this.scrollableElement.scrollTop = parseInt(this.localStorageService.getItem('scrollTop')) | 0
+        //     document.getElementById(this.localStorageService.getItem('id'))?.classList.add('p-highlight')
+        // }, 1000)
     }
 
     private loadRecords(): Promise<any> {
@@ -152,7 +204,7 @@ export class ScheduleListComponent {
             const listResolved: ListResolved = this.activatedRoute.snapshot.data[this.feature]
             if (listResolved.error === null) {
                 this.records = listResolved.list
-                this.recordsFiltered = listResolved.list
+                this.recordsFilteredCount = this.records.length
                 resolve(this.records)
             } else {
                 this.goBack()
@@ -163,12 +215,20 @@ export class ScheduleListComponent {
     }
 
     private populateDropdownFilters(): void {
-        this.dropdownDestinations = this.helperService.getDistinctRecords(this.records, 'destinationDescription')
-        this.dropdownPorts = this.helperService.getDistinctRecords(this.records, 'portDescription')
+        this.distinctDestinations = this.helperService.getDistinctRecords(this.records, 'destination', 'description')
+        this.distinctPorts = this.helperService.getDistinctRecords(this.records, 'port', 'description')
     }
 
     private setLocale(): void {
         this.dateAdapter.setLocale(this.localStorageService.getLanguage())
+    }
+
+    private storeSelectedId(id: number): void {
+        this.localStorageService.saveItem('id', id.toString())
+    }
+
+    private storeScrollTop(): void {
+        this.localStorageService.saveItem('scrollTop', this.scrollableElement.scrollTop)
     }
 
     private subscribeToInteractionService(): void {
@@ -176,6 +236,10 @@ export class ScheduleListComponent {
             this.formatDatesToLocale()
             this.setLocale()
         })
+    }
+
+    private toggleVirtual(): void {
+        // this.isVirtual = !this.isVirtual
     }
 
     //#endregion
